8edb5d8b5dcee379f586778f3d828adb03d44dd3
vincentlinzhu <vincentlinzhu11@gmail.com>
2024-06-07 18:19:36 +0000

    initial commit

---
+++ nanogpt/nanogpt_module.py
@@ -0,0 +1,121 @@
+"""
+Sample from a trained model
+"""
+from pathlib import Path
+from contextlib import nullcontext
+import torch
+from nanogpt.model import GPTConfig, GPT
+
+
+class NanoGptPlayer:
+    def __init__(self, model_name: str):
+        self.model_name = model_name
+        # -----------------------------------------------------------------------------
+
+        seed = 1337
+        # device = "cuda"  # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+        device = "cpu"
+        dtype = "float16"  # 'float32' or 'bfloat16' or 'float16'
+        compile = False  # use PyTorch 2.0 to compile the model to be faster
+        # -----------------------------------------------------------------------------
+
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
+        torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
+        device_type = (
+            "cuda" if "cuda" in device else "cpu"
+        )  # for later use in torch.autocast
+        ptdtype = {
+            "float32": torch.float32,
+            "bfloat16": torch.bfloat16,
+            "float16": torch.float16,
+        }[dtype]
+        ctx = (
+            nullcontext()
+            if device_type == "cpu"
+            else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+        )
+
+        # model
+        ckpt_path = f"{self.model_name}"
+        checkpoint = torch.load(ckpt_path, map_location=device)
+        gptconf = GPTConfig(**checkpoint["model_args"])
+        model = GPT(gptconf)
+        state_dict = checkpoint["model"]
+        unwanted_prefix = "_orig_mod."
+        for k, v in list(state_dict.items()):
+            if k.startswith(unwanted_prefix):
+                state_dict[k[len(unwanted_prefix) :]] = state_dict.pop(k)
+        model.load_state_dict(state_dict)
+
+        model.eval()
+        model.to(device)
+        if compile:
+            model = torch.compile(model)  # requires PyTorch 2.0 (optional)
+
+        meta = {
+            'vocab_size': 32,
+            'itos': {
+                0: ' ', 1: '#', 2: '+', 3: '-', 4: '.', 5: '0', 6: '1', 7: '2', 8: '3',
+                9: '4', 10: '5', 11: '6', 12: '7', 13: '8', 14: '9', 15: ';', 16: '=',
+                17: 'B', 18: 'K', 19: 'N', 20: 'O', 21: 'Q', 22: 'R', 23: 'a', 24: 'b',
+                25: 'c', 26: 'd', 27: 'e', 28: 'f', 29: 'g', 30: 'h', 31: 'x'
+            },
+            'stoi': {
+                ' ': 0, '#': 1, '+': 2, '-': 3, '.': 4, '0': 5, '1': 6, '2': 7, '3': 8,
+                '4': 9, '5': 10, '6': 11, '7': 12, '8': 13, '9': 14, ';': 15, '=': 16,
+                'B': 17, 'K': 18, 'N': 19, 'O': 20, 'Q': 21, 'R': 22, 'a': 23, 'b': 24,
+                'c': 25, 'd': 26, 'e': 27, 'f': 28, 'g': 29, 'h': 30, 'x': 31
+            }
+        }
+        # TODO want to make this more general to arbitrary encoder/decoder schemes
+        stoi, itos = meta["stoi"], meta["itos"]
+        encode = lambda s: [stoi[c] for c in s]
+        decode = lambda l: "".join([itos[i] for i in l])
+
+        self.encode = encode
+        self.decode = decode
+        self.model = model
+        self.ctx = ctx
+        self.device = device
+
+    def get_nanogpt_response(self, game_state: str, temperature: float) -> str:
+        num_samples = 1  # number of samples to draw
+        top_k = 200  # retain only the top_k most likely tokens, clamp others to have 0 probability
+        max_new_tokens = 10
+
+        game_state = ";" + game_state
+        
+        # print('MODEL_INPUT:', game_state)
+
+        start_ids = self.encode(game_state)
+
+        x = torch.tensor(start_ids, dtype=torch.long, device=self.device)[None, ...]
+        with torch.no_grad():
+            with self.ctx:
+                for k in range(num_samples):
+                    y = self.model.generate(
+                        x, max_new_tokens, temperature=temperature, top_k=top_k
+                    )
+
+                    model_response = self.decode(y[0].tolist())
+
+        model_response = model_response[len(game_state) :]
+        if ";" in model_response:
+            model_response = model_response.split(";")[0]
+        return model_response
+
+    def get_move_from_response(self, response: str) -> str:
+        # Parse the response to get only the first move
+        moves = response.split()
+        first_move = moves[0]
+
+        return first_move
+
+    def get_move(self, game_state: str, temperature: float) -> str:
+        completion = self.get_nanogpt_response(game_state, temperature)
+        return self.get_move_from_response(completion)
+
+    def get_config(self) -> dict:
+        return {"model": self.model_name}
---
+++ nanogpt_engine.py
@@ -0,0 +1,177 @@
+"""
+Some example classes for people who want to create a homemade bot.
+
+With these classes, bot makers will not have to implement the UCI or XBoard interfaces themselves.
+"""
+
+from __future__ import annotations
+import os
+import re
+import chess
+from chess.engine import PlayResult
+import logging
+from homemade import ExampleEngine
+from lib.engine_wrapper import FillerEngine, check_for_draw_offer, get_book_move, get_egtb_move, get_online_move, move_time
+from nanogpt.nanogpt_module import NanoGptPlayer
+import chess.engine
+import chess.polyglot
+import chess.syzygy
+import chess.gaviota
+import logging
+import datetime
+import time
+import test_bot.lichess
+from lib import config, model, lichess
+from lib.timer import Timer, to_seconds
+from typing import Any, Optional, Union
+OPTIONS_TYPE = dict[str, Any]
+MOVE_INFO_TYPE = dict[str, Any]
+COMMANDS_TYPE = list[str]
+LICHESS_EGTB_MOVE = dict[str, Any]
+CHESSDB_EGTB_MOVE = dict[str, Any]
+MOVE = Union[chess.engine.PlayResult, list[chess.Move]]
+LICHESS_TYPE = Union[lichess.Lichess, test_bot.lichess.Lichess]
+
+from dataclasses import dataclass
+
+
+@dataclass
+class LegalMoveResponse:
+    move_san: Optional[str] = None
+    move_uci: Optional[chess.Move] = None
+    attempts: int = 0
+    is_resignation: bool = False
+    is_illegal_move: bool = False
+
+# Use this logger variable to print messages to the console or log files.
+# logger.info("message") will always print "message" to the console or log file.
+# logger.debug("message") will only print "message" if verbose logging is enabled.
+logger = logging.getLogger(__name__)
+
+
+# Return is (move_san, move_uci, attempts, is_resignation, is_illegal_move)
+def get_legal_move(
+    player: NanoGptPlayer,
+    board: chess.Board,
+    game_state: str,
+    max_attempts: int = 10,
+# ) -> LegalMoveResponse:
+) -> PlayResult:
+    """Request a move from the player and ensure it's legal."""
+    move_san = None
+    move_uci = None
+
+
+    for attempt in range(max_attempts):
+        move_san = player.get_move(game_state, float(os.environ["TEMPERATURE"]))
+        # player.get_move(
+        #     board, game_state, min(((attempt / max_attempts) * 1) + 0.001, temperature)
+        # )
+
+        # Sometimes when GPT thinks it's the end of the game, it will just output the result
+        # Like "1-0". If so, this really isn't an illegal move, so we'll add a check for that.
+        if move_san is not None:
+            if move_san == "1-0" or move_san == "0-1" or move_san == "1/2-1/2":
+                print(f"{move_san}, player has resigned")
+                # Does not break the lichess-bot because an empty play result will be noted as a resignation
+                # chess.engine.EngineError('Player has resigned')
+                resginationMove = PlayResult("", "")
+                resginationMove.resigned = True
+                return resginationMove #!
+
+        try:
+            move_uci = board.parse_san(move_san)
+            return PlayResult(move_uci, None)            
+            
+        except Exception as e:
+            print(f"Error parsing move {move_san}: {e}")
+            continue
+
+    if move_uci is None:
+        raise chess.engine.EngineError('Failed to find legal move')
+
+
+
+class NanoGPTEngine(ExampleEngine):
+    def __init__(self, model_name: str, commands: COMMANDS_TYPE, options: OPTIONS_TYPE, stderr: Optional[int],
+                 draw_or_resign: config.Configuration, game: Optional[model.Game] = None, name: Optional[str] = None, temperature: float = os.environ["TEMPERATURE"],
+                 **popen_args: str):
+        self.player = NanoGptPlayer(model_name = model_name)
+        self.temperature = temperature
+        # super().__init__(options, draw_or_resign) # there are no options or draw_or_resign values in the config.yml file
+
+        self.engine_name = self.__class__.__name__ if name is None else name
+
+        self.engine = FillerEngine(self, name=self.engine_name)
+
+    def search(self, board: chess.Board) -> PlayResult: 
+        is_black = board.turn == chess.BLACK
+        
+        raw_pgn = str(chess.pgn.Game.from_board(board))        
+        # > print(raw_pgn)
+        # [Event "?"]
+        # [Site "?"]
+        # [Date "????.??.??"]
+        # [Round "?"]
+        # [White "?"]
+        # [Black "?"]
+        # [Result "*"]
+
+        # 1. Nc3 e5 2. e4 Nf6 *
+        
+        game_state = re.sub(r'\. |\s+', lambda m: '.' if m.group(0) == '. ' else ' ', raw_pgn.split('\n\n')[1]) # this will remove spaces after game number
+        # > print(game_state)
+        # 1.Nc3 e5 2.e4 Nf6 *
+        
+        game_state = game_state.replace('*', '') if is_black else game_state.replace('*', f'{board.fullmove_number}.') # removes the *
+        # 1.Nc3 e5 2.e4 Nf6 
+        return get_legal_move(self.player, board, game_state)
+        
+    def play_move(self,
+                  board: chess.Board,
+                  game: model.Game,
+                  li: LICHESS_TYPE,
+                  setup_timer: Timer,
+                  move_overhead: datetime.timedelta,
+                  can_ponder: bool,
+                  is_correspondence: bool,
+                  correspondence_move_time: datetime.timedelta,
+                  engine_cfg: config.Configuration,
+                  min_time: datetime.timedelta) -> None:
+        """
+        Play a move.
+
+        :param board: The current position.
+        :param game: The game that the bot is playing.
+        :param li: Provides communication with lichess.org.
+        :param start_time: The time that the bot received the move.
+        :param move_overhead: The time it takes to communicate between the engine and lichess.org.
+        :param can_ponder: Whether the engine is allowed to ponder.
+        :param is_correspondence: Whether this is a correspondence or unlimited game.
+        :param correspondence_move_time: The time the engine will think if `is_correspondence` is true.
+        :param engine_cfg: Options for external moves (e.g. from an opening book), and for engine resignation and draw offers.
+        :param min_time: Minimum time to spend, in seconds.
+        :return: The move to play.
+        """
+
+        try:
+            best_move = self.search(board)
+        except chess.engine.EngineError as error:
+            BadMove = (chess.IllegalMoveError, chess.InvalidMoveError)
+            # if any(isinstance(e, BadMove) for e in error.args):
+                # logger.error("Ending game due to bot attempting an illegal move.")
+            game_ender = li.abort if game.is_abortable() else li.resign
+            game_ender(game.id)
+            raise
+
+        # Heed min_time
+        elapsed = setup_timer.time_since_reset()
+        if elapsed < min_time:
+            time.sleep(to_seconds(min_time - elapsed))
+
+        # self.add_comment(best_move, board) # my nanogpt doesn't have a comment function I guess
+        # self.print_stats()
+        if best_move.resigned and len(board.move_stack) >= 2:
+            li.resign(game.id)
+        else:
+            li.make_move(game.id, best_move)
